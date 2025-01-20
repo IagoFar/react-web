7fe3be1d7c4c361ac23d970e95c8bee019a67fa4
IagoFar <neila.galiza@hotmail.com>
2025-01-20 19:22:00 +0100

    Updated _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,10 @@
 export const config = {
     compatibility_date: "2024-01-01",
     compatibility_flags: ["nodejs_compat"]
-  };
+};
+
+export default {
+    async fetch(request, env, ctx) {
+      return await env.ASSETS.fetch(request);
+    }
+  };  
